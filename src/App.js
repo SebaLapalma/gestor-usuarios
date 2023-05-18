cedff399ec6d205768719e727af310bcb36f7cb6
SebaLapalma <e.sebastian.lapalma@gmail.com>
2023-05-18 08:19:01 -0300

    Refactorización de código en App. Adjunción de componente para formulario y uso de custom hook dentro del mismo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import useFormulario from './hooks/useFormulario'
-import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
-import Button from './components/Button'
 import { useState } from 'react'
+import UserForm from './components/UserForm'
 
 function App() {
   const [usuarios, setUsuarios] = useState([])
   
-  const [formulario, handleChange, reset] = useFormulario({
-    name: '',
-    lastname: '',
-    email: ''
-  })
-
-  const submit = (e) => {
-    e.preventDefault()
+  
+  const submit = (usuario) => {
     setUsuarios([
       ...usuarios,
-      formulario,
+      usuario,
     ])
-    reset()
   }
 
   return (
@@ -28,30 +19,7 @@ function App() {
       <Container>
         <Card>
           <div style={{ padding: 20 }}>
-            <form onSubmit={submit}>
-              <Input
-                label='Nombre'
-                name='name'
-                value={formulario.name}
-                onChange={handleChange}
-                placeholder='Nombre'
-              />
-              <Input
-                label='Apellido'
-                name='lastname'
-                value={formulario.lastname}
-                onChange={handleChange}
-                placeholder='Apellido'
-              />
-              <Input
-                label='Correo'
-                name='email'
-                value={formulario.email}
-                onChange={handleChange}
-                placeholder='Correo'
-              />
-              <Button>Enviar</Button>
-            </form>
+            <UserForm submit={submit} />
           </div>
         </Card>
         <Card>
